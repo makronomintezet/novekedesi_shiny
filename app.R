202204c5f5ef26f3f2e38db1e271d06a8ddec301
Granát Marcell <mgranat@makronom.org>
2021-07-13 17:01:45 +0200

    2021-07-13

--- app.R
+++ app.R
@@ -3,8 +3,9 @@ library(shinymanager)
 library(shinydashboard)
 
 components <- readRDS("components.rds")
+data_descript <- readRDS("data_descript.RDS")
 source('functions.R', encoding = 'UTF-8')
-data_descript <- readxl::read_excel("adatok_forrasa_leirasa.xlsx")
+# data_descript <- readxl::read_excel("adatok_forrasa_leirasa.xlsx")
 
 
 dbHeader <- dashboardHeader(title = "Inkluzív növekedés",
@@ -56,8 +57,8 @@ ui <- secure_app(head_auth = tags$script(inactivity),
                                  tabItems(
                                    # First tab content
                                    tabItem(tabName = "data",
-                                           box(title = "Felhasznált adatok", height = "400px", width = 12,
-                                               DT::dataTableOutput("descriptive", height = "300px")
+                                           box(title = "Felhasznált adatok", height = "1700px", width = 12,
+                                               shiny::tableOutput("descriptive")
                                            )
                                            
                                    ),
@@ -77,12 +78,13 @@ ui <- secure_app(head_auth = tags$script(inactivity),
                                    tabItem(tabName = "wef",
                                            box(
                                              # Title can include an icon
-                                             title = "WEF jellegű ábrák", width = 12, height = "1300px",
+                                             title = "WEF jellegű ábrák", width = 12, height = "780px",
                                              shiny::inputPanel(
                                                selectInput("development_wef",
-                                                           "Fejletség:", choices = c("Fejlett", "Fejlődő", "Fejlődő (alsóbb)"))
+                                                           "Fejletség:", choices = c("Fejlett", "Fejlődő", "Fejlődő (alsóbb)")),
+                                               selectInput("wef_dimension", "Dimenzió", choices = c("Fenntarthatóság", "Munkaalapú társadalom", "Gazdasági fejlettség és növekedés", "Társadalmi egyenlőtlenség"))
                                              ),
-                                             shiny::plotOutput("wef_plot", height = "1100px")
+                                             shiny::plotOutput("wef_plot", height = "600px")
                                              
                                            )
                                    ),
@@ -94,9 +96,12 @@ ui <- secure_app(head_auth = tags$script(inactivity),
                                                  selectInput("y",
                                                              "Y-tengely:", choices = NiceName(names(components[-c(1:3)])), selected = "Függőségi ráta"),
                                                  selectInput("size",
-                                                             "Méret:", choices = NiceName(names(components[-c(1:3)])), selected = "GDP (PPP)")
+                                                             "Méret:", choices = NiceName(names(components[-c(1:3)])), selected = "GDP (PPP)"),
+                                                 selectInput("dynamic_development",
+                                                             "Fejlettség:", choices = c("Összes", "Fejlett", "Fejlődő", "Fejlődő (alsóbb)")),
+                                                 sliderInput("dynamic_time", "Év:", 2010, 2019, step = 1, sep = "", value = 2019, animate = TRUE)
                                                ), 
-                                               plotly::plotlyOutput("dynamic_plot")
+                                               shiny::plotOutput("dynamic_plot")
                                            )
                                    )
                                  )
@@ -113,31 +118,32 @@ server <- function(input, output) {
   })
   
   
-  output$descriptive <- DT::renderDataTable({
-    data_descript %>% 
-      select(1:3) %>% 
-      arrange(Adat) %>% 
-      replace_na(list(Leírás = "")) %>% 
-      mutate(Leírás = str_wrap(Leírás, 30)) %>% 
-      DT::datatable(
-        extensions = c('Buttons', "Scroller"),
-        options = list(
-          deferRender = TRUE,
-          scrollY = 800,
-          scroller = TRUE,
-          style = "bootstrap4",
-          searching = TRUE,
-          autoWidth = TRUE,
-          ordering = TRUE,
-          dom = 'Bfrtip',
-          buttons = c('copy', 'csv', 'excel', 'pdf', 'print')
-        )
-      )
-  })
-  
-  
-  output$box1 <- shinydashboard::renderInfoBox({
+  output$descriptive <- shiny::renderTable({
     
+    data_descript 
+    # %>%
+      # arrange(Adat) %>%
+      # replace_na(list(Leírás = "")) %>%
+      # mutate(Leírás = str_wrap(Leírás, 30)) %>%
+      # DT::datatable(
+      #   extensions = c('Buttons', "Scroller"),
+      #   options = list(
+      #     deferRender = TRUE,
+      #     scrollY = 300,
+      #     scroller = TRUE,
+      #     style = "bootstrap4",
+      #     searching = TRUE,
+      #     autoWidth = TRUE,
+      #     ordering = TRUE,
+      #     dom = 'Bfrtip',
+      #     buttons = c('copy', 'csv', 'excel', 'pdf', 'print')
+      #   )
+      # )
+  })
+
+
+  output$box1 <- shinydashboard::renderInfoBox({
+    # TODO WORKS
     development_filter <- case_when(
       input$development == "Fejlett" ~ "advanced_economies",
       input$development == "Fejlődő" ~ "emerging_economies",
@@ -145,18 +151,18 @@ server <- function(input, output) {
       TRUE ~ c("advanced_economies", "emerging_economies", "emerging_economies_cont_d")
     )
     
-    out <- components %>% 
-      pivot_longer(-c(1:3)) %>% 
-      mutate(observed = !is.na(value)) %>% 
-      filter(NiceName(name) == input$choosen_var) %>% 
+    out <- components %>%
+      pivot_longer(-c(1:3)) %>%
+      mutate(observed = !is.na(value)) %>%
+      filter(NiceName(name) == input$choosen_var) %>%
       {
         if (input$development != "összes") {
           filter(., development == development_filter)
         } else {
           .
         }
-      } %>% 
-      {mean(.$observed)} %>% 
+      } %>%
+      {mean(.$observed)} %>%
       percent_hu(accuracy = 1)
     
     infoBox(
@@ -174,24 +180,24 @@ server <- function(input, output) {
       TRUE ~ c("advanced_economies", "emerging_economies", "emerging_economies_cont_d")
     )
     
-    components %>% 
-      pivot_longer(-c(1:3)) %>% 
+    components %>%
+      pivot_longer(-c(1:3)) %>%
       mutate(
         name = NiceName(name),
         observed = ifelse(!is.na(value), "elérhető", "nem elérhető")
-      ) %>% 
-      filter(name == input$choosen_var) %>% 
+      ) %>%
+      filter(name == input$choosen_var) %>%
       {
         if (input$development != "összes") {
           filter(., development == development_filter)
         } else {
           .
         }
-      } %>% 
-      ggplot(aes(country, time, fill = observed)) + 
+      } %>%
+      ggplot(aes(country, time, fill = observed)) +
       geom_tile(color = "black") +
-      theme_minimal() + 
-      scale_y_continuous(labels = as.character, breaks = 2010:2020) +
+      theme_minimal() +
+      scale_y_continuous(labels = as.character, breaks = 2010:2019) +
       labs(x = "Ország", y = "Év", fill = NULL) +
       theme(
         axis.title = element_text(size = 14),
@@ -199,7 +205,7 @@ server <- function(input, output) {
         axis.text.x = element_text(angle = 40, hjust = .4, face = "bold"),
         legend.position = "bottom",
         legend.text = element_text(size = 20)
-      ) + 
+      ) +
       scale_fill_manual(values = c("elérhető" = "steelblue","nem elérhető" = "red2"))
   })
   
@@ -211,14 +217,21 @@ server <- function(input, output) {
       TRUE ~ c("advanced_economies", "emerging_economies", "emerging_economies_cont_d")
     )
     
+    dimension_filter <- case_when(
+      input$wef_dimension == "Fenntarthatóság" ~ "sustainability",
+      input$wef_dimension == "Munkaalapú társadalom" ~ "work",
+      input$wef_dimension == "Gazdasági fejlettség és növekedés" ~ "economic",
+      input$wef_dimension == "Társadalmi egyenlőtlenség" ~ "equality"
+    )
+    
     components %>%
-      arrange(time) %>% 
-      group_by(country) %>% 
-      fill(everything()) %>% 
-      ungroup() %>% 
-      filter(time == 2020) %>% 
-      select(-gdp_ppp, -population) %>% 
-      pivot_longer(-c(1:3), names_to = "variable") %>% 
+      arrange(time) %>%
+      group_by(country) %>%
+      fill(everything()) %>%
+      ungroup() %>%
+      filter(time == 2019) %>%
+      select(-gdp_ppp, -population) %>%
+      pivot_longer(-c(1:3), names_to = "variable") %>%
       group_by(variable, development) %>%
       group_modify(~ mutate(.x, value_rank = GetRank(value))) %>%
       ungroup() %>%
@@ -226,55 +239,70 @@ server <- function(input, output) {
         value_rank = ifelse(is.na(value), NA, value_rank),
         value_rank = ifelse(add_direction(variable) == "good", value_rank, 5 + 1 - value_rank),
         value_rank = factor(value_rank, levels = 1:5, ordered = T),
-        dimension = add_dimension(variable, hun = TRUE)
-      ) %>% 
-      filter(development == development_filter) %>% 
+        dimension = add_dimension(variable, hun = FALSE)
+      ) %>%
+      filter(dimension == dimension_filter) %>%
+      filter(development == development_filter) %>%
       ggplot() +
-      aes(country, variable, fill = value_rank, label = value) + 
-      geom_tile(color = "black") + 
-      scale_y_discrete(labels = NiceName) + 
+      aes(country, variable, fill = value_rank, label = value) +
+      geom_tile(color = "black", size = .8) +
+      scale_y_discrete(labels = NiceName) +
       scale_fill_brewer(palette = "Spectral", guide = guide_legend(nrow = 1, label.position = "bottom"),
                         label = c("Legrosszabb 20%", "", "", "", "Legjobb 20%", "Nincs adat"),
                         na.value = "grey") +
       labs(y = NULL, x = NULL, fill = NULL) +
-      facet_wrap(~ dimension, scales = "free_y", ncol = 1) +
       theme(
         strip.text = element_text(size = 13, face = "bold"),
         axis.text.x = element_text(angle = 90, vjust = .4),
         axis.text = element_text(size = 14),
-        legend.key.width = unit(2, "cm"),
+        legend.text = element_text(size = 14),
+        legend.key.width = unit(3, "cm")
       )
   })
   
-  output$dynamic_plot <- plotly::renderPlotly({
+  output$dynamic_plot <- shiny::renderPlot({
     x_axis <- recode_name(input$x, FALSE)
     y_axis <- recode_name(input$y, FALSE)
     input_size <- recode_name(input$size, FALSE)
+
+    development_filter <- case_when(
+      input$development_wef == "Fejlett" ~ "advanced_economies",
+      input$development_wef == "Fejlődő" ~ "emerging_economies",
+      input$development_wef == "Fejlődő (alsóbb)" ~ "emerging_economies_cont_d",
+      TRUE ~ "all"
+    )
+
+    df <- components %>%
+      select(development, time, country, x_axis, y_axis, input_size) %>%
+      set_names("development", "time", "country", "x", "y", "size")
     
-    p <- components %>% 
-      select(development, time, country, x_axis, y_axis, input_size) %>% 
-      set_names("development", "Év", "country", "x", "y", "size") %>% 
-      mutate(
-        development = NiceName(development),
-        text = str_c("Ország: ", country, "\n",
-                     NiceName(x_axis), ": ", format(x, digits = 2), "\n",
-                     NiceName(y_axis), ": ", format(y, digits = 2), "\n",
-                     NiceName(input_size), ": ", format(size, digits = 2)
-        )
-      ) %>% 
-      ggplot() +
-      aes(x = x, y = y, fill = country, size = size, frame = Év, text = text) + 
+    plot_limits <- list(
+      xmin = min(df$x),
+      xmax = max(df$x),
+      ymin = min(df$y),
+      ymax = max(df$y)
+    )
+
+    if (development_filter != "all") {
+      df <- df %>%
+        filter(development == development_filter)
+    }
+    
+    df <- df  %>%
+      filter(time == input$dynamic_time)
+    
+    ggplot(df) +
+      aes(x = x, y = y, fill = country, size = size) + # TODO modify the frame title
       geom_point(show.legend = FALSE) +
       scale_size(range = c(5, 20)) +
-      facet_wrap(~ development) +
-      labs(x = input$x, y = input$y) + 
-      theme(legend.position = "none", 
+      labs(x = input$x, y = input$y) +
+      theme(legend.position = "none",
             strip.text = element_text(size = 20),
-            axis.text = element_text(size = 14), 
+            axis.text = element_text(size = 14),
             axis.title = element_text(size = 16)
-      )
-    
-    myplotly(p, height = 700, width = 1900)
+      ) + 
+      scale_x_continuous(limits = c(plot_limits$xmin, plot_limits$xmax)) +
+      scale_y_continuous(limits = c(plot_limits$ymin, plot_limits$ymax))
   })
   
 }
